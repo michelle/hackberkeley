0b4eb6999d8e84792bb9ebbc7c8877f676083466
ericz <really.ez@gmail.com>
2011-10-16 12:27:59 -0700

    blah

--- server.js
+++ server.js
@@ -15,6 +15,22 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
+app.get('/sponsors', function(req, res){
+  res.render('sponsors');
+});
+
+
+app.get('/projects', function(req, res){
+  res.render('projects');
+});
+
+app.get('/people', function(req, res){
+  res.render('people');
+});
+
+app.get('/media', function(req, res){
+  res.render('media');
+});
 
 app.listen(80);
 
