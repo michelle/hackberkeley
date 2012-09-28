fc190b9e91178a026789ee9097262d117dcb8f44
Michelle Bu <analogmidnight@gmail.com>
2012-09-27 22:51:16 -0700

    added some stuff

--- app.js
+++ app.js
@@ -266,6 +266,8 @@ app.get('/media/:id', function(req, res){
 
 });
 
+app.get('/hack', function(req, res) {
+  res.render('hack', {layout: false});
+});
+
 app.listen(process.env.PORT || 8086);
-
-
